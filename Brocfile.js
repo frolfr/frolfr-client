e871c2c95809f6a345dd30e901df6b7c8a3c5f94
Jordan Killpack <jordan@bignerdranch.com>
2015-02-24 17:25:33 -0500

    Add font awesome

--- Brocfile.js
+++ Brocfile.js
@@ -23,9 +23,16 @@ app.import('bower_components/typeahead.js/dist/typeahead.jquery.js');
 app.import('bower_components/highcharts/highcharts.js');
 app.import('bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js');
 app.import('bower_components/bootstrap-sass-official/assets/stylesheets/_bootstrap.scss');
+app.import('bower_components/components-font-awesome/css/font-awesome.css');
+
 var bootstrapFonts = pickFiles('bower_components/bootstrap-sass-official/assets/fonts', {
     srcDir: '/',
     destDir: '/fonts'
 });
 
-module.exports = app.toTree([bootstrapFonts]);
+var fontAwesome = pickFiles('bower_components/components-font-awesome/fonts', {
+    srcDir: '/',
+    destDir: '/fonts'
+});
+
+module.exports = app.toTree([bootstrapFonts, fontAwesome]);
