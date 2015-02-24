a0098db6006966df7e787615f55cf483ec8f3f15
Jordan Killpack <jordan@bignerdranch.com>
2015-02-24 16:56:04 -0500

    Fix logging out

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,11 +1,10 @@
 import Ember from 'ember';
-import App from "../app";
 
 var ApplicationRoute = Ember.Route.extend({
   actions: {
     logout: function() {
       this.controllerFor('sessions').reset();
-      App.reset();
+      window.location = "/sessions";
     }
   }
 });
