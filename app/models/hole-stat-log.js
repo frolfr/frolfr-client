4e6d306646cd6090e04a90a24303800dd4029fdd
Geoff Schorkopf <gschorkopf@gmail.com>
2015-03-31 15:07:46 -0400

    Writes a simple view with an ugly table to see stats
    It works

--- app/models/hole-stat-log.js
+++ app/models/hole-stat-log.js
@@ -8,6 +8,14 @@ var HoleStatLog = DS.Model.extend({
   averagePar: DS.attr('number'),
   averageShooting: DS.attr('number'),
   ranking: DS.attr('number'),
+
+  displayAverageScore: function() {
+    return Number(this.get('averageScore')).toFixed(3);
+  }.property('averageScore'),
+
+  displayAverageShooting: function() {
+    return Number(this.get('averageShooting')).toFixed(3);
+  }.property('averageShooting'),
 });
 
 export default HoleStatLog;
