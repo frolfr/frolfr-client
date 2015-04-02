2c2481ae51a6721bc6f9393fb1705120399fbaa5
Geoff Schorkopf <gschorkopf@gmail.com>
2015-04-02 15:24:22 -0400

    Prettifies and adds conditional to "Load More" button

--- app/controllers/friends.js
+++ app/controllers/friends.js
@@ -4,6 +4,10 @@ import pagedArray from 'ember-cli-pagination/computed/paged-array';
 var FriendsController = Ember.ArrayController.extend({
   pagedContent: pagedArray("content", {infinite: true}),
 
+  hasNextPage: function() {
+    return this.get('content.totalPages') > this.get('content.page');
+  }.property('content.page', 'content.totalPages'),
+
   actions: {
     loadNext: function() {
       this.get('pagedContent').loadNextPage();
