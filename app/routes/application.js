4a70afcfab3c9ca0b1ee2e178ca98b531997616b
Geoff Schorkopf <gschorkopf@gmail.com>
2015-04-02 20:18:24 -0400

    Updates application route to add nextUnplayedHole

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,6 +5,7 @@ var ApplicationRoute = Ember.Route.extend({
     logout: function() {
       this.controllerFor('sessions').reset();
       this.controllerFor('application').set('currentRound', null);
+      this.controllerFor('application').set('currentHole', null);
       window.location = "/sessions";
     }
   },
@@ -15,6 +16,9 @@ var ApplicationRoute = Ember.Route.extend({
 
     this.store.find('round', 'current').then(function(round) {
       _this.controllerFor('application').set('currentRound', round);
+
+      var nextUnplayedHole = round.get('scorecards.firstObject.nextUnplayedTurn.holeNumber');
+      _this.controllerFor('application').set('currentHole', nextUnplayedHole);
     });
   }
 });
