13081cf6f6d381d8d3fd3fa606a4877f2a6af1ba
Geoff Schorkopf <gschorkopf@gmail.com>
2015-04-06 11:44:50 -0400

    Paginates the courses index route

--- app/controllers/courses/index.js
+++ app/controllers/courses/index.js
@@ -1,8 +1,12 @@
 import Ember from 'ember';
+import pagedArray from 'ember-cli-pagination/computed/paged-array';
 
 var CoursesIndexController = Ember.ArrayController.extend({
-  sortProperties: ['name'],
-  sortAscending: true
+  queryParams: ["page"],
+
+  pageBinding: "content.page",
+
+  page: 1
 });
 
 export default CoursesIndexController;
