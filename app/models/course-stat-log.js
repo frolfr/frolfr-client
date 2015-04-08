b8326bf50a2ba9be65f001f9d137ced9b5458037
Geoff Schorkopf <gschorkopf@gmail.com>
2015-04-08 09:54:44 -0500

    Adds average score / shooting to course-stat-log

--- app/models/course-stat-log.js
+++ app/models/course-stat-log.js
@@ -5,11 +5,21 @@ var CourseStatLog = DS.Model.extend({
   completedScorecardsPlayed: DS.attr('number'),
   averageScoreOnCompletedScorecards: DS.attr('number'),
   bestScoreOnCompletedScorecards: DS.attr('number'),
+  averageShootingOnCompletedScorecards: DS.attr('number'),
+  bestShootingOnCompletedScorecards: DS.attr('number'),
   totalBogeys: DS.attr('number'),
   totalPars: DS.attr('number'),
   totalBirdies: DS.attr('number'),
   totalEagles: DS.attr('number'),
   totalDoublesOrWorse: DS.attr('number'),
+
+  displayAverageScore: function() {
+    return Number(this.get('averageScoreOnCompletedScorecards')).toFixed(3);
+  }.property('averageScoreOnCompletedScorecards'),
+
+  displayAverageShooting: function() {
+    return Number(this.get('averageShootingOnCompletedScorecards')).toFixed(3);
+  }.property('averageShootingOnCompletedScorecards'),
 });
 
 export default CourseStatLog;
