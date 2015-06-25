5b726ab412922621de51c042809e4020cc1f3d5a
Geoffrey Schorkopf <gschorkopf@gmail.com>
2015-06-25 14:48:48 -0400

    Adds some helper computed properties

--- app/models/profile-stat-log.js
+++ app/models/profile-stat-log.js
@@ -9,6 +9,18 @@ var ProfileStatLog = DS.Model.extend({
   topPlayedCourseScorecardsStarted: DS.attr('number'),
   topFriendName: DS.attr('string'),
   topFriendRoundsCount: DS.attr('number'),
+
+  hasNeverPlayed: function() {
+    return this.get('scorecardsStarted') === 0;
+  }.property('scorecardsStarted'),
+
+  displayTopPlayedCourse: function() {
+    return this.get('topPlayedCourseName') + ' (' + this.get('topPlayedCourseScorecardsStarted') + ' rounds)';
+  }.property('topPlayedCourseName', 'topPlayedCourseScorecardsStarted'),
+
+  displayTopFriend: function() {
+    return this.get('topFriendName') + ' (' + this.get('topFriendRoundsCount') + ' rounds)';
+  }.property('topFriendName', 'topFriendRoundsCount')
 });
 
 export default ProfileStatLog;
