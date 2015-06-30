c816ffdf499a82ed74deef7b8b154b742f11c43c
Geoffrey Schorkopf <gschorkopf@gmail.com>
2015-06-29 20:51:21 -0400

    Writes true/false tests for hasRatings

--- tests/unit/controllers/course-rounds-test.js
+++ tests/unit/controllers/course-rounds-test.js
@@ -2,14 +2,25 @@ import {
   moduleFor,
   test
 } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleFor('controller:course-rounds', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
 });
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  var controller = this.subject();
-  assert.ok(controller);
+test('hasRatings', function(assert) {
+  var controller = this.subject(),
+      scorecard = Ember.Object.create({rating: 1000});
+
+  controller.set('filteredContent', [scorecard]);
+
+  assert.ok(controller.get('hasRatings'), 'when any scorecard has a rating');
+});
+
+test('!hasRatings', function(assert) {
+  var controller = this.subject(),
+      scorecard = Ember.Object.create({rating: null});
+
+  controller.set('filteredContent', [scorecard]);
+
+  assert.equal(controller.get('hasRatings'), false, 'when no scorecard has a rating');
 });
