a97c3fa0de840a1d26fc60f78e8b7a2fdf04c045
Geoffrey Schorkopf <gschorkopf@gmail.com>
2015-06-30 22:04:24 -0400

    Rewrites rounds table as an actual table
    * Had to write a small component for the strokes cells as well
    [#62]

---
+++ app/components/strokes-table-cell.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+var StrokesTableCellComponent = Ember.Component.extend({
+  text: function() {
+    return this.get('turn.displayStrokes');
+  }.property('turn.displayStrokes'),
+
+  className: function() {
+    return this.get('turn.parStatus');
+  }.property('turn.parStatus'),
+
+  turn: function() {
+    var holeNumber = this.get('holeNumber').toString();
+
+    return this.get('scorecard').get('turns').find(function(turn) {
+      return turn.get('holeNumber') === holeNumber;
+    });
+  }.property('scorecard.turns.@each', 'holeNumber')
+});
+
+export default StrokesTableCellComponent;
