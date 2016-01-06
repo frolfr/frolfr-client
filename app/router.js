34853a4971b5ff9c2e710aea91791b7b9f5a577a
Patrick Dougall <pdougall1@gmail.com>
2016-01-06 13:43:09 -0500

    Keeps some changes after ember init

--- app/router.js
+++ app/router.js
@@ -1,12 +1,9 @@
 import Ember from 'ember';
 import config from './config/environment';
-var Router = Ember.Router.extend({
+
+const Router = Ember.Router.extend({
   location: config.locationType
 });
-// Router.reopen({
-//   location: 'auto',
-//   rootURL: '/'
-// });
 
 Router.map(function() {
   this.route('welcome');
