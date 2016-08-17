20cca9b41b6ca4049e5c00802cd0212ba877331e
Geoffrey Schorkopf <gschorkopf@gmail.com>
2016-08-17 16:32:30 -0400

    Prefers 'query' to 'find' with hashed params
    http://emberjs.com/blog/2015/06/18/ember-data-1-13-released.html#toc_que
    ry-and-queryrecord

--- app/routes/hole-stat-logs.js
+++ app/routes/hole-stat-logs.js
@@ -2,7 +2,7 @@ import AuthenticatedRoute from './authenticated';
 
 var HoleStatLogsRoute = AuthenticatedRoute.extend({
   model: function() {
-    return this.store.find('holeStatLog', { course_id: this.modelFor('course').id });
+    return this.store.query('holeStatLog', { course_id: this.modelFor('course').id });
   }
 });
 
